bec1f7f2d3c1af9947e1228f8bc1d1b6559fab4f
Azura Jamaludin <2022888198@student.uitm.edu.my>
2026-06-03 14:48:40 +0800

    Added About page GitHub URL

--- app/src/main/java/com/example/wattwise/AboutActivity.java
+++ app/src/main/java/com/example/wattwise/AboutActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,8 @@ public class AboutActivity extends AppCompatActivity {
 
     LinearLayout btnGithub;
     TextView txtGithub;
+
+    String githubUrl = "https://github.com/Ict602/WattWise";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,22 @@ public class AboutActivity extends AppCompatActivity {
         btnGithub = findViewById(R.id.btnGithub);
         txtGithub = findViewById(R.id.txtGithub);
 
-        String githubUrl = "https://github.com/azurajamaludin/WattWise";
         txtGithub.setText(githubUrl);
 
-        btnGithub.setOnClickListener(v -> {
+        btnGithub.setOnClickListener(v -> openGithub());
+        txtGithub.setOnClickListener(v -> openGithub());
+    }
+
+    private void openGithub() {
+        try {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(githubUrl));
             startActivity(intent);
-        });
+        } catch (Exception e) {
+            Toast.makeText(
+                    AboutActivity.this,
+                    "Unable to open GitHub link",
+                    Toast.LENGTH_SHORT
+            ).show();
+        }
     }
 }
